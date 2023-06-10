c43d51b2b3ff0bbaf44f6f1a1cd96642a206084f
HRojotiana <hei.rojotiana@gmail.com>
2023-06-10 12:56:49 +0300

    dark mode modifications

--- public/js/themes.js
+++ public/js/themes.js
@@ -1,7 +1,10 @@
 const button = document.getElementById("theme-btn");
 const bodyMessage = document.getElementById("body-message");
+const body = document.getElementById("body");
 
 
 button.addEventListener("click", ()=>{
     bodyMessage.classList.toggle("dark");
+
+    body.classList.toggle("dark");
 });
